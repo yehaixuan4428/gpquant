38a7ee5e8f8581c982a73233b23abd5b96571243
yehaixuan_xs <yehaixuan_xs@chinastock.com.cn>
2024-07-10 14:57:52 +0800

    update test file

--- test.py
+++ test.py
@@ -77,163 +77,163 @@ def get_data(online=False):
 def test_functions(data):
     from gpquant.Function import _clear_by_cond
 
-    print(_clear_by_cond(data['close'], data['open'], data['high']))
+    print(_clear_by_cond(data["close"], data["open"], data["high"]))
 
     from gpquant.Function import _if_then_else
 
-    print(_if_then_else(data['close'], data['open'], data['high']))
+    print(_if_then_else(data["close"], data["open"], data["high"]))
 
     from gpquant.Function import _if_cond_then_else
 
-    print(_if_cond_then_else(data['close'], data['open'], data['high'], data['low']))
+    print(_if_cond_then_else(data["close"], data["open"], data["high"], data["low"]))
 
     from gpquant.Function import _ts_delay
 
-    print(_ts_delay(data['close'], 3))
+    print(_ts_delay(data["close"], 3))
 
     from gpquant.Function import _ts_delta
 
-    print(_ts_delta(data['close'], 3))
+    print(_ts_delta(data["close"], 3))
 
     from gpquant.Function import _ts_pct_change
 
-    print(_ts_pct_change(data['close'], 3))
+    print(_ts_pct_change(data["close"], 3))
 
     from gpquant.Function import _ts_mean_return
 
-    print(_ts_mean_return(data['close'], 3))
+    print(_ts_mean_return(data["close"], 3))
 
     from gpquant.Function import _ts_max
 
-    print(_ts_max(data['close'], 3))
+    print(_ts_max(data["close"], 3))
 
     from gpquant.Function import _ts_min
 
-    print(_ts_min(data['close'], 3))
+    print(_ts_min(data["close"], 3))
 
     from gpquant.Function import _ts_sum
 
-    print(_ts_sum(data['close'], 3))
+    print(_ts_sum(data["close"], 3))
 
     from gpquant.Function import _ts_product
 
-    print(_ts_product(data['close'], 3))
+    print(_ts_product(data["close"], 3))
 
     from gpquant.Function import _ts_mean
 
-    print(_ts_mean(data['close'], 3))
+    print(_ts_mean(data["close"], 3))
 
     from gpquant.Function import _ts_std
 
-    print(_ts_std(data['close'], 3))
+    print(_ts_std(data["close"], 3))
 
     from gpquant.Function import _ts_median
 
-    print(_ts_median(data['close'], 3))
+    print(_ts_median(data["close"], 3))
 
     from gpquant.Function import _ts_midpoint
 
-    print(_ts_midpoint(data['close'], 3))
+    print(_ts_midpoint(data["close"], 3))
 
     from gpquant.Function import _ts_skew
 
-    print(_ts_skew(data['close'], 3))
+    print(_ts_skew(data["close"], 3))
 
     from gpquant.Function import _ts_kurt
 
-    print(_ts_kurt(data['close'], 3))
+    print(_ts_kurt(data["close"], 3))
 
     from gpquant.Function import _ts_inverse_cv
 
-    print(_ts_inverse_cv(data['close'], 3))
+    print(_ts_inverse_cv(data["close"], 3))
 
     from gpquant.Function import _ts_cov
 
-    print(_ts_cov(data['close'], data['open'], 3))
+    print(_ts_cov(data["close"], data["open"], 3))
 
     from gpquant.Function import _ts_corr
 
-    print(_ts_corr(data['close'], data['open'], 3))
+    print(_ts_corr(data["close"], data["open"], 3))
 
     from gpquant.Function import _ts_autocorr
 
-    print(_ts_autocorr(data['close'], 10, 1))
+    print(_ts_autocorr(data["close"], 10, 1))
 
     from gpquant.Function import _ts_maxmin
 
-    print(_ts_maxmin(data['close'], 3))
+    print(_ts_maxmin(data["close"], 3))
 
     from gpquant.Function import _ts_zscore
 
-    print(_ts_zscore(data['close'], 3))
+    print(_ts_zscore(data["close"], 3))
 
     from gpquant.Function import _ts_regression_beta
 
-    print(_ts_regression_beta(data['close'], data['open'], 3))
+    print(_ts_regression_beta(data["close"], data["open"], 3))
 
     from gpquant.Function import _ts_linear_slope
 
-    print(_ts_linear_slope(data['close'], 3))
+    print(_ts_linear_slope(data["close"], 3))
 
     from gpquant.Function import _ts_linear_intercept
 
-    print(_ts_linear_intercept(data['close'], 3))
+    print(_ts_linear_intercept(data["close"], 3))
 
     from gpquant.Function import _ts_argmax
 
-    print(_ts_argmax(data['close'], 3))
+    print(_ts_argmax(data["close"], 3))
 
     from gpquant.Function import _ts_argmin
 
-    print(_ts_argmin(data['close'], 3))
+    print(_ts_argmin(data["close"], 3))
 
     from gpquant.Function import _ts_argmaxmin
 
-    print(_ts_argmaxmin(data['close'], 3))
+    print(_ts_argmaxmin(data["close"], 3))
 
     from gpquant.Function import _ts_rank
 
-    print(_ts_rank(data['close'], 3))
+    print(_ts_rank(data["close"], 3))
 
     from gpquant.Function import _ts_ema
 
-    print(_ts_ema(data['close'], 3))
+    print(_ts_ema(data["close"], 3))
 
     from gpquant.Function import _ts_dema
 
-    print(_ts_dema(data['close'], 3))
+    print(_ts_dema(data["close"], 3))
 
     from gpquant.Function import _ts_kama
 
-    print(_ts_kama(data['close'], 10, 5, 3))
+    print(_ts_kama(data["close"], 10, 5, 3))
 
     from gpquant.Function import _ts_AROONOSC
 
-    print(_ts_AROONOSC(data['high'], data['low'], 10))
+    print(_ts_AROONOSC(data["high"], data["low"], 10))
 
     from gpquant.Function import _ts_WR
 
-    print(_ts_WR(data['high'], data['low'], data['close'], 10))
+    print(_ts_WR(data["high"], data["low"], data["close"], 10))
 
     from gpquant.Function import _ts_CCI
 
-    print(_ts_CCI(data['high'], data['low'], data['close'], 10))
+    print(_ts_CCI(data["high"], data["low"], data["close"], 10))
 
     from gpquant.Function import _ts_ATR
 
-    print(_ts_ATR(data['high'], data['low'], data['close'], 10))
+    print(_ts_ATR(data["high"], data["low"], data["close"], 10))
 
     from gpquant.Function import _ts_NATR
 
-    print(_ts_NATR(data['high'], data['low'], data['close'], 10))
+    print(_ts_NATR(data["high"], data["low"], data["close"], 10))
 
     from gpquant.Function import _ts_ADX
 
-    print(_ts_ADX(data['high'], data['low'], data['close'], 10))
+    print(_ts_ADX(data["high"], data["low"], data["close"], 10))
 
     from gpquant.Function import _ts_MFI
 
-    print(_ts_MFI(data['high'], data['low'], data['close'], data['volume'], 10))
+    print(_ts_MFI(data["high"], data["low"], data["close"], data["volume"], 10))
 
 
 def test_fit(data):
@@ -272,11 +272,33 @@ def test_fit(data):
     print(sr.score(test_data, test_data["close"]))
 
 
+def test_tree():
+    from gpquant.SyntaxTree import SyntaxTree
+
+    tree = SyntaxTree(
+        0,
+        init_depth=(3, 3),
+        init_method="half and half",
+        function_set=list(function_map.values()),
+        variable_set=["open", "high", "low", "close", "volume"],
+        const_range=(1, 5),
+        ts_const_range=(1, 5),
+        build_preference=[0.75, 0.75],
+        metric="sectional ic",
+        transformer=None,
+        transformer_kwargs=None,
+        parsimony_coefficient=0.005,
+    )
+    print(tree)
+
+
 if __name__ == "__main__":
     # rqdatac.init()
     # data = get_data(online=True)
     # data.to_pickle("data.pkl")
-    data = pd.read_pickle("data.pkl")
+    # data = pd.read_pickle("data.pkl")
 
     # test_functions(data)
-    test_fit(data)
+    # test_fit(data)
+
+    test_tree()
